d1990a67221a1ca6d1a01b008c1c340fdc223284
adampodkowinski <adampodkdev@gmail.com>
2020-08-06 21:36:47 +0200

    Add Game class and change project name

---
+++ Game.cpp
@@ -0,0 +1,15 @@
+//
+// Created by PC on 2020-08-06.
+//
+
+#include "Game.h"
+
+Game::~Game()
+{
+
+}
+
+Game::Game()
+{
+
+}
---
+++ Game.h
@@ -0,0 +1,16 @@
+//
+// Created by PC on 2020-08-06.
+//
+
+#pragma once
+
+class Game
+{
+public:
+    Game();
+
+    virtual ~Game();
+};
+
+
+
